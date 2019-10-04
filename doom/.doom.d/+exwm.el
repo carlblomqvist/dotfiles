673e89148159efca9e804d65e41c7918138124fc
Carl Blomqvist <carl.blomqvist@gmail.com>
2019-10-04 16:48:51 +0200

    Add exwm

---
+++ doom/.doom.d/+exwm.el
@@ -0,0 +1,12 @@
+;;; exwm --- Summary
+;;; Commentary:
+;;; +exwm.el -*- lexical-binding: t; -*-
+
+;;; Code:
+(require 'exwm-config)
+(exwm-config-default)
+
+
+
+(provide '+exwm)
+;;; +exwm.el ends here
