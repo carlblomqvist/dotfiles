80aae41a031b7878c7422cdfea08f03899a28cff
Carl Blomqvist <carl.blomqvist@gmail.com>
2019-12-31 12:27:13 +0100

    Increase font to a sensible HDPI size again

--- doom/.doom.d/+ui.el
+++ doom/.doom.d/+ui.el
@@ -2,7 +2,7 @@
 
 (setq doom-theme 'doom-one) ;; doom-opera is also nice
 
-(setq doom-font (font-spec :family "Fira Code" :size 14))
+(setq doom-font (font-spec :family "Fira Code" :size 26))
 (setq-default doom-big-font (font-spec :family "Fira Code" :size 42))
 
 (setq display-line-numbers-type 'relative)
