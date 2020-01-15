d83cc0696267e8bd18d24226783d54e53475015c
Carl Blomqvist <carl.blomqvist@gmail.com>
2020-01-15 23:01:33 +0100

    Add option to specify a single directory to install

--- install.sh
+++ install.sh
@@ -2,12 +2,24 @@
 #
 # Install dotfiles using stow dynamically.
 #
-# Created by Dylan Araps.
+# Created by Dylan Araps, improved by Carl Blomqvist
 
-for dir in */; do
-    child=("$dir"*); root=
-    [[ "$child" =~ $dir(bin|etc|var|usr|opt) ]] && root="root required"
-    read -rn 1 -p "install ${dir/\/*}${root:+ ($root)}? [y/n] " ans; echo
-    [[ "${ans,,}" != "y" ]] && continue
-    [[ "$root" ]] && { sudo stow "$dir" -t /;:; } || stow "$dir"
-done
+if [ $# -eq 0 ]
+then
+    for dir in */; do
+        child=("$dir"*); root=
+        [[ "$child" =~ $dir(bin|etc|var|usr|opt) ]] && root="root required"
+        read -rn 1 -p "install ${dir/\/*}${root:+ ($root)}? [y/n] " ans; echo
+        [[ "${ans,,}" != "y" ]] && continue
+        [[ "$root" ]] && { sudo stow "$dir" -t /;:; } || stow "$dir"
+    done
+else
+    for dir in "$@"; do
+        dir="$dir/"
+        child=("$dir"*); root=
+        [[ "$child" =~ $dir(bin|etc|var|usr|opt) ]] && root="root required"
+        read -rn 1 -p "install ${dir/\/*}${root:+ ($root)}? [y/n] " ans; echo
+        [[ "${ans,,}" != "y" ]] && continue
+        [[ "$root" ]] && { sudo stow "$dir" -t /;:; } || stow "$dir"
+    done
+fi
