fefdc0e005d9ab83d46062a4975752a19996bf18
Carl Blomqvist <carl.blomqvist@gmail.com>
2020-02-17 00:40:20 +0100

    Add copy to clipboard functionality

--- i3/.config/i3/screenshot.sh
+++ i3/.config/i3/screenshot.sh
@@ -2,11 +2,19 @@
 
 DATE=$(echo `date '+%Y-%m-%d-%H:%M:%S'`.jpg)
 FILE=home/carlb/pictures/screenshots/"$DATE"
+copy_and_delete='sh -c "xclip -selection clipboard -t image/png -i $f; rm $f"'
 
 if [ "$1" = "-s" ]
 then
-    scrot -s ~/pictures/screenshots/"$DATE"
-    notify-send "Screenshot Saved as $DATE"
+    if [ "$2" = "-c" ]
+    then
+        scrot -f -s -e "$copy_and_delete"
+        continue
+    else
+        bindsym --release $mod+Shift+p exec
+        scrot -s ~/pictures/screenshots/"$DATE"
+        notify-send "Screenshot Saved as $DATE"
+    fi
 else
     scrot ~/pictures/screenshots/"$DATE"
     notify-send "Screenshot Saved as $DATE"
