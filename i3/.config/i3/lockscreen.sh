a31f35b422e8ae9285032287f2c40263f229f98f
Carl Blomqvist <carl.blomqvist@gmail.com>
2020-02-17 00:40:07 +0100

    Don't fork

--- i3/.config/i3/lockscreen.sh
+++ i3/.config/i3/lockscreen.sh
@@ -3,4 +3,4 @@
 scrot /tmp/lockscreen.png
 #convert /tmp/lockscreen.png -blur 0x8 lockscreen_blur.png
 convert /tmp/lockscreen.png -paint 8 /tmp/lockscreen_blur.png
-i3lock -i /tmp/lockscreen_blur.png
+i3lock -n -i /tmp/lockscreen_blur.png
