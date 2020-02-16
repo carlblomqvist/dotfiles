f2eb64d84882c92b3a3cb8facacc4e2e96ad45f8
Carl Blomqvist <carl.blomqvist@gmail.com>
2020-02-17 00:38:45 +0100

    Update gpg-key

--- doom/.doom.d/+magit.el
+++ doom/.doom.d/+magit.el
@@ -7,7 +7,7 @@
                                        ("/home/carlb/dotfiles/" . 0))
         ;; magit-display-file-buffer-function #'switch-to-buffer-other-window
         magithub-clone-default-directory "~/git" ;; I want my stuff to clone to ~/git
-        magit-commit-arguments '("--gpg-sign=A253CD35E5042635C409B4DC9A9BFD4AAD2FC01C")
-        magit-rebase-arguments '("--autostash" "--gpg-sign=A253CD35E5042635C409B4DC9A9BFD4AAD2FC01C")
-        magit-pull-arguments '("--rebase" "--autostash" "--gpg-sign=A253CD35E5042635C409B4DC9A9BFD4AAD2FC01C")
+        magit-commit-arguments '("--gpg-sign=A1068EEF8AEC5895199D2D9A45816912C2542AC7")
+        magit-rebase-arguments '("--autostash" "--gpg-sign=A1068EEF8AEC5895199D2D9A45816912C2542AC7")
+        magit-pull-arguments '("--rebase" "--autostash" "--gpg-sign=A1068EEF8AEC5895199D2D9A45816912C2542AC7")
         magithub-preferred-remote-method 'ssh_url)) ;; HTTPS cloning is awful, i authenticate with ssh keys.
