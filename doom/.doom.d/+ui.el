480442ee1514181d9d623b20c93ad09636d51130
Carl Blomqvist <carl.blomqvist@gmail.com>
2020-03-06 09:52:29 +0100

    Change theme to nord

--- doom/.doom.d/+ui.el
+++ doom/.doom.d/+ui.el
@@ -1,6 +1,6 @@
 ;;; ~/.doom.d/+ui.el -*- lexical-binding: t; -*-
 
-(setq doom-theme 'doom-one) ;; doom-opera is also nice
+(setq doom-theme 'doom-nord) ;; doom-opera is also nice
 
 (setq doom-font (font-spec :family "Fira Code" :size 26))
 (setq-default doom-big-font (font-spec :family "Fira Code" :size 42))
